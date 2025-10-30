664f52e8ed71bdb104031463e1b132ff5298e4cb
anthonyanup21 <anthonyanup21@gmail.com>
2025-10-30 15:16:17 +0530

    added controller and middlewares

---
+++ backend/models/task.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    assignedTo: {
+      type: String,
+      required: true,
+    },
+  
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
